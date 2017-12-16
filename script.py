a3559be386c2ce3f4482544b2b9cd436b9668576
Michał Budzyński <michal@virtualdesign.pl>
2017-12-16 15:38:00 +0100

    :thumbsdown: simple object manipulation

--- script.py
+++ script.py
@@ -1,10 +1,20 @@
 import bpy
+import mathutils
 
-directory = "./dist/"
+DIST_DIR = "./dist/"
 
 # print(dir(bpy))
 
 def save_file(name):
-	bpy.ops.wm.save_as_mainfile(filepath=directory+name)
+	bpy.ops.wm.save_as_mainfile(filepath=DIST_DIR+name)
+
+cube = bpy.data.objects["Cube"]
+# one blender unit in x-direction
+vec = mathutils.Vector((1.0, 5.0, 0.0))
+inv = cube.matrix_world.copy()
+inv.invert()
+# vec aligned to local axis
+vec_rot = vec * inv
+cube.location = cube.location + vec_rot
 
 save_file('test.blend')
